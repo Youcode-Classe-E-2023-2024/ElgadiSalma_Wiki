cd5d8fd7e9a77d84822ef800c04c4ba254b530ad
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-08 14:21:43 +0100

    Update register_controller.php

--- controllers/register_controller.php
+++ controllers/register_controller.php
@@ -1,26 +1,47 @@
-<?php
+    <?php
 
-include_once 'models/User.php';
+    include_once 'models/User.php';
 
-$users = new  User();
+    $users = new  User();
 
 
-if($_SERVER["REQUEST_METHOD"] === 'POST')
-{
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password=$_POST['password'];
-    $photo=$_POST['photo'];
-    $email_err= "";
-
-    if($users->check_email($email))
+    if($_SERVER["REQUEST_METHOD"] === 'POST')
     {
-        $email_err="Email deja existant";
-    }
-    else
-    {
-        if(empty($email_err))
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password=$_POST['password'];
+        $photo = $_FILES['photo']['name'];
+        $email_err= "";
+
+        if($users->check_email($email))
         {
+            $email_err="Email deja existant";
+        }
+        else
+        {
+                // upload pic
+                $targetDir = "./assets/image/users/";
+                // Get the uploaded file's name and temporary name
+                $photo = basename($_FILES["photo"]["name"]);
+                $tmpName = $_FILES["photo"]["tmp_name"];
+                // Set the target path
+                $targetPath = $targetDir . $photo;
+                // Check if the file is an image
+                $imageFileType = strtolower(pathinfo($targetPath, PATHINFO_EXTENSION));
+                $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+                if (in_array($imageFileType, $allowedExtensions)) 
+                {
+                    // Move the file to the target directory
+                    if (move_uploaded_file($tmpName, $targetPath)) 
+                    {
+                        // echo "The file " . $photo . " has been uploaded.";
+                    } else {
+                        echo "Sorry, there was an error uploading your file.";
+                    }
+                } else {
+                    echo "Sorry, only JPG, JPEG, PNG, and GIF files are allowed.";
+                }
+                // end upload pic
             if ($users->register($email, $username, $password, $photo)) 
             {
             header("location:index.php?page=login");
@@ -29,7 +50,6 @@ if($_SERVER["REQUEST_METHOD"] === 'POST')
             {
                 header("location:<?= PATH ?>views/register.php?error");
             }
-        }
+            }
 
     }
-}
