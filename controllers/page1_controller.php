12f0c484ee2a6183ee86f974d8a7b5314cea35b0
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-08 11:21:54 +0100

    Create page1_controller.php

---
+++ controllers/page1_controller.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once 'models/Room.php';
+
+$Room = new  Room();
+
+$myId = $_SESSION['id_user'];
+// echo $myId;
+
+
+if (isset($_POST['logout'])) 
+{
+    // echo'hh';
+    session_start();
+    $_SESSION = array();
+    session_destroy();
+    header("Location:index.php?page=login");
+    exit();
+
+}
+
+$rooms = $Room->getRooms($myId);
+
+
+
