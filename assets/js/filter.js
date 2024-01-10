4c32578e4143ce0ce87d7241866131af95d97695
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-10 11:22:01 +0100

    Update filter.js

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -5,11 +5,11 @@ document.addEventListener('DOMContentLoaded', function ()
 
     var categorySelect = document.getElementById('category');
     var tagSelect = document.getElementById('tag');
-    var searchInput = document.getElementById('searchInput');
+    const searchInput = document.getElementById('searchInput');
+
 
     categorySelect.addEventListener('change', fetchDataAndDisplay);
     tagSelect.addEventListener('change', fetchDataAndDisplay);
-    searchInput.addEventListener('keyup', Search);
 
     fetchDataAndDisplay();
 
@@ -22,19 +22,22 @@ document.addEventListener('DOMContentLoaded', function ()
 
         fetch(`index.php?page=filter&category=${category}&tag=${tag}`)
             .then(response => response.json())
-            .then(tickets => {
-                ticketData = tickets;
-                displayData(tickets);
+            .then(wikis => {
+                wikiData = wikis; 
+                displayData(wikis);
             })
             .catch(error => console.error('Error', error));
     }
 
-    function Search() 
+    searchInput.addEventListener('keyup', function () 
     {
         const searchTerm = searchInput.value.trim().toLowerCase();
-        const filteredWikis = wikiData.filter(wiki => wiki.titre.toLowerCase().includes(searchTerm));
+        console.log(wikiData)
+        const filteredWikis = wikiData.filter(wiki => wiki.title.toLowerCase().includes(searchTerm));
+    
         displayData(filteredWikis);
-    }
+    });
+    
 
     function displayData(wikis) 
     {
@@ -45,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function ()
                 `
                 <div class=" text-center  w-content ">
                 <div class="max-w-sm border-2 bg-white px-6  pt-6 flex flex-col justify-between pb-2 rounded-xl shadow-lg transform transition duration-500">
-                    <h3 class="mb-3 text-xl font-bold text-indigo-600">${wiki.titre}</h3>
+                    <h3 class="mb-3 text-xl font-bold text-indigo-600">${wiki.title}</h3>
                     <img class="w-full rounded-xl h-72 w-72" src="./assets/image/wikis/${wiki.photo}" />
                     <div class="">
                             <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Voir plus -></button>
