7f9a91b2733d7836838b8d3d9c56ad0eed10550b
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-10 10:21:40 +0100

    Update filter.js

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         console.log('Fetching data - category:', category, 'tag:', tag);
 
-        fetch(`../../controller/Ticket/filter_tickets.php?category=${category}&tag=${tag}`)
+        fetch(`index.php?page=filter&category=${category}&tag=${tag}`)
             .then(response => response.json())
             .then(tickets => {
                 ticketData = tickets;
@@ -43,12 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 <div class="max-w-sm bg-white px-6 pt-6 flex flex-col justify-between pb-2 rounded-xl shadow-lg transform transition duration-500">
                     <h3 class="mb-3 text-xl font-bold text-indigo-600">${wiki.titre}</h3>
                     <img class="w-full rounded-xl h-72 w-72" src="./assets/image/wikis/${wiki.photo}" />
-                    <div class="mt-4 flex gap-2">
-                        <a href="<?= PATH ?>index.php?page=edit_wiki?id=1" class="inline-block w-1/2 rounded-md bg-green-500 px-6 py-2 font-semibold text-green-100 shadow-md duration-75 hover:bg-green-400"><button type="submit" name="modifier" >Modifier</button></a>
-                        <form action="<?= PATH ?>index.php?page=wikis" method="post">
-                            <input type="hidden" name="wikiId" value="${wiki.id_wiki}">
-                            <button type="submit" name="supprimer" class="inline-block rounded-md bg-red-500 px-10 py-2 font-semibold text-red-100 shadow-md duration-75 hover:bg-red-400">Supprimer</button>
-                        </form>
+                    <div class="mt-4 flex gap-2">                    
                     </div>
                     <div class="">
                         <div class="flex space-x-1 items-center">
@@ -56,7 +51,6 @@ document.addEventListener('DOMContentLoaded', function () {
                         </div>
                     </div>
                 </div>
-            
                 `
             );
         });
