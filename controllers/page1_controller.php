fc3da77ba9c52b7f4bdf38912b92841198160ef5
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-10 12:19:33 +0100

    Update page1_controller.php

--- controllers/page1_controller.php
+++ controllers/page1_controller.php
@@ -1,6 +1,7 @@
 <?php
 
 $Wiki = new  Wiki();
+$User = new  User();
 
 // display all categories
 $categories = $Wiki->getAllCategories();
@@ -11,6 +12,18 @@ $tags = $Wiki->getAllTags();
 //end display all tags
 
 
+// graphe
+
+// number of users
+$countUsers = $User->countUsers();
+
+// number of wikis
+$countWikis = $Wiki->countWikis();
+
+
+
+
+// end graphe
 
 $myId = $_SESSION['id_user'];
 // echo $myId;
