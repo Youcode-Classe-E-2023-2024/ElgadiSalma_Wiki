46cdd4cf625249a48ccf67817ebad9bc2dd9fe0d
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-10 10:21:32 +0100

    Update page1_controller.php

--- controllers/page1_controller.php
+++ controllers/page1_controller.php
@@ -1,5 +1,17 @@
 <?php
 
+$Wiki = new  Wiki();
+
+// display all categories
+$categories = $Wiki->getAllCategories();
+//end display all categories
+
+// display all tags
+$tags = $Wiki->getAllTags();
+//end display all tags
+
+
+
 $myId = $_SESSION['id_user'];
 // echo $myId;
 
