629226f14bcb833b5d1b0a808c4492b496c830f4
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-10 01:02:51 +0100

    Update filter.js

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -39,14 +39,24 @@ document.addEventListener('DOMContentLoaded', function () {
         const rows = wikis.map((wiki) => {
             return (
                 `
-                    <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12" style="width: 30%;" >
-                        <div class="mb-12 space-y-4">
-                            <h3 class="text-2xl font-semibold text-purple-900"> ${wiki.titre}</h3>
-                            <p class="mb-6 whitespace-pre">${wiki.description}</p>
-                            <a href="./description.php?id=${wiki.id_wiki}" class="block font-medium text-purple-600">Know more</a>
-                        </div>
-                        <img src="https://tailus.io/sources/blocks/end-image/preview/images/ux-design.svg" class="w-2/3 ml-auto" alt="illustration" loading="lazy" width="900" height="600">
+
+                <div class="max-w-sm bg-white px-6 pt-6 flex flex-col justify-between pb-2 rounded-xl shadow-lg transform transition duration-500">
+                    <h3 class="mb-3 text-xl font-bold text-indigo-600">${wiki.titre}</h3>
+                    <img class="w-full rounded-xl h-72 w-72" src="./assets/image/wikis/${wiki.photo}" />
+                    <div class="mt-4 flex gap-2">
+                        <a href="<?= PATH ?>index.php?page=edit_wiki?id=1" class="inline-block w-1/2 rounded-md bg-green-500 px-6 py-2 font-semibold text-green-100 shadow-md duration-75 hover:bg-green-400"><button type="submit" name="modifier" >Modifier</button></a>
+                        <form action="<?= PATH ?>index.php?page=wikis" method="post">
+                            <input type="hidden" name="wikiId" value="${wiki.id_wiki}">
+                            <button type="submit" name="supprimer" class="inline-block rounded-md bg-red-500 px-10 py-2 font-semibold text-red-100 shadow-md duration-75 hover:bg-red-400">Supprimer</button>
+                        </form>
                     </div>
+                    <div class="">
+                        <div class="flex space-x-1 items-center">
+                            <button class="mt-2 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Voir plus -></button>
+                        </div>
+                    </div>
+                </div>
+            
                 `
             );
         });
