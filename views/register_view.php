22a911f67381fbd6a70a431a48077f3729947cf9
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-11 12:32:31 +0100

    Update register_view.php

--- views/register_view.php
+++ views/register_view.php
@@ -7,20 +7,35 @@
     </div>
     <form class="p-12 md:p-24" action="<?= PATH ?>index.php?page=register" method="post" enctype="multipart/form-data">
       <!-- username -->
+      <?php
+        if(!empty($username_err)){
+        ?>
+        <span class="text-red-700">*<?php echo $username_err; ?></span>
+        <?php } ?> 
       <div class="flex items-center text-lg mb-6 md:mb-8">
         <svg class="absolute ml-3" width="24" viewBox="0 0 24 24">
           <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z"/>
         </svg>
-        <input type="text" name="username" class="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Username" required/>
+        <input type="text" name="username" class="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Username" />
       </div>
       <!-- photo -->
+      <?php
+        if(!empty($photo_err)){
+        ?>
+        <span class="text-red-700">*<?php echo $photo_err; ?></span>
+        <?php } ?> 
       <div class="flex items-center text-lg mb-6 md:mb-8">
         <svg class="absolute ml-3" width="24" viewBox="0 0 24 24">
           <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z"/>
         </svg>
-        <input type="file" name="photo" class="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Photo de profil" required/>
+        <input type="file" name="photo" class="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Photo de profil" />
       </div>
       <!-- email -->
+      <?php
+        if(!empty($email_err)){
+        ?>
+        <span class="text-red-700">*<?php echo $email_err; ?></span>
+        <?php } ?> 
       <div class="flex items-center text-lg mb-6 md:mb-8">
         <svg class="absolute ml-3" width="24" viewBox="0 0 24 24">
           <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z"/>
